c075a21fc58b998e798c95e2c950fb753e79751d
IrfnGnwn <irfngnwn.0202@gmail.com>
2024-11-03 21:42:09 +0700

    commit ketiga

--- resources/views/pelanggan/layout/index.blade.php
+++ resources/views/pelanggan/layout/index.blade.php
@@ -11,47 +11,61 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <style>
-        .icon-nav{
-            color: white;
-        }
-        .icon-nav:hover{
-            color: black;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/custome.css') }}">
     <title>Online Shop | {{ $title }}</title>
 </head>
 <body>
     <main>
         <header>
-            <nav class="navbar navbar-dark navbar-expand-lg" style="background-color: #025464">
-                <div class="container">
-                  <a class="navbar-brand" href="#">Bampres Shop</a>
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="collapse navbar-collapse justify-content-end gap-4" id="navbarSupportedContent">
-                    <ul class="navbar-nav gap-4">
-                      <li class="nav-item">
-                        <a class="nav-link fs-5 active" aria-current="page" href="#">Home</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link fs-5" href="#">Shop</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link fs-5" href="#">Contact Us</a>
-                      </li>
-                    </ul>
-                    <div class="d-flex gap-4 align-items-center">
-                        <button class="btn btn-success" type="button">Login | Register</button>
-                        <a href="#" class="fs-5 icon-nav">
-                            <i class="fa-solid fa-bag-shopping"></i>
-                        </a>
-                    </div>
-                  </div>
-                </div>
-              </nav>
+          @include('pelanggan.component.navbar')
         </header>
+        <section>
+          ini kontennya guys
+        </section>
+        <footer>
+          <div class="container">
+            <div class="d-flex gap-4">
+              <div class="title-left">
+                <div class="header-title">
+                  Us3 Shop
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque deleniti incidunt libero eum,
+                   eos laborum tempora expedita voluptatibus dolor soluta eligendi harum quas ducimus molestiae
+                   sed repellat, ratione esse maiores!</p>
+              </div>
+              <div class="title-middle">
+                <div class="header-title">
+                  Tentang Kami
+                </div>
+                <ul>
+                  <li>
+                    <address>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, soluta aut.
+                      Facilis accusamus saepe dolor!
+                    </address>
+                  </li>
+                  <li>
+                    <p>089612902227</p>
+                  </li>
+                  <li>
+                    abcdefg12345
+                  </li>
+                </ul>
+              </div>
+              <div class="title-sosmed">
+                <div class="header-title">
+                  Sosial Media
+                </div>
+                <div class="sosmed">
+                  <i class="fa-brands fa-instagram"></i>
+                  <i class="fa-brands fa-square-facebook"></i>
+                  <i class="fa-brands fa-whatsapp"></i>
+                  <i class="fa-brands fa-linkedin"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
     </main>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
